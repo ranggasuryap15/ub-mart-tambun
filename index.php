80c9350e13faa8668bb4806e35cf7f005bc48547
reyhand-ebs <mohamad.reyhand.f@students.esqbs.ac.id>
2022-11-09 21:49:11 +0700

    indexing pages, and give bootstrap link

--- index.php
+++ index.php
@@ -1,5 +1,46 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-echo "UB Mart Tambun"
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-?>
+    <!-- NANTI PAGENYA GAUSAH INISIALISASI LINK2NYA, SCRIPTNYA, METANYA, DLL DIDALEM HEAD SELAIN TITLE AJA YAA!!. Contohnya kyk page home -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-7mQhpDl5nRA5nY9lr8F1st2NbIly/8WqhjTp+0oFxEA/QUuvlbF6M1KXezGBh3Nb" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href=css/style.css">
+</head>
+
+<body>
+    <!-- NAVIGATION BAR -->
+    <nav>
+
+    </nav>
+
+    <!-- BODY -->
+    <?php
+    $pages_dir = 'pages';
+    if (!empty($_GET['p'])) {
+        $pages = scandir($pages_dir, 0);
+        unset($pages[0], $pages[1]);
+
+        $p = $_GET['p'];
+        if (in_array($p . '.php', $pages)) {
+            include($pages_dir . '/' . $p . '.php');
+        } else {
+            echo 'Halaman tidak ditemukan! :(';
+        }
+    } else {
+        include($pages_dir . '/home.php');
+    }
+    ?>
+
+    <!-- FOOTER -->
+    <footer>
+
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ pages/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Home</title>
+</head>
+
+<body>
+    <div class="container">
+        <p>Ini adalah Home Page</p>
+    </div>
+</body>
+
+</html>

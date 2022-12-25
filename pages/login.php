30b7a23a7e1da0b35fe752da555ded8f31ae0988
ranggasuryap15 <ranggasurya.313@gmail.com>
2022-12-25 22:21:08 +0700

    delete

--- pages/login.php
+++ pages/login.php
@@ -1,8 +1,5 @@
 <?php
 session_start();
-// if (!isset($_SESSION['username'])) {
-//     header("location:/pages/login.php");
-// }
 require_once (__DIR__ . "/../App/Class/Akun.php");
 $akun = new Akun;
 
